874118a7dd1a26b18d3ac959bb5466d0bbd66545
SMK <idsiemu@gmail.com>
2019-12-10 21:35:00 +0900

    해당 유저 프로필에 갔을 때 내가 그사람을 팔로우했냐 안했냐를 판별해줌

--- src/api/User/computed.js
+++ src/api/User/computed.js
@@ -5,25 +5,21 @@ export default {
         fullName: (parent) => {
             return `${parent.firstName} ${parent.lastName}`;
         },
-        amIFollowing: async(parent, _, { request }) => {
+        isFollowing: (parent, _, { request }) => {
             const { user } = request;
             const { id: parentId } = parent;
             try{
-                const exists = await prisma.$exists.user({
-                    AND: [{id:parentId}, {followers_some: [user.id]}]
+                return prisma.$exists.user({
+                    //AND연산을 해서 ture false를 반환하는데 여기서의 조건은 내가 해당 유저를 팔라우 하고있냐?를 판별하는 것이다.
+                    //그래서 user.id는 나를 뜻하고 parentId 는 해당 프로필을 보고하자는 대상을 말한다.
+                    AND: [{id:user.id}, {following_some: {id: parentId}}]
                 });
-                if(exists) {
-                    return true;
-                }else{
-                    return false;
-                }
             }catch(error){
                 console.log(error);
                 return false;
             }
-            
         },
-        itsMe: (parent, _, { request }) => {
+        isSelf: (parent, _, { request }) => {
             const { user } = request;
             const { id: parentId } = parent;
             return user.id === parentId;
